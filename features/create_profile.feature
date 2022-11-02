8a2c09f84bb08fd23816bc011e2fc5f5d3c62b2a
Jack Wang <jackwang@Jacks-MacBook-Pro-5.local>
2022-11-02 19:42:57 -0400

    spec tests done and some cucumber tests

--- features/create_profile.feature
+++ features/create_profile.feature
@@ -5,28 +5,31 @@ Feature: create a new user profile
 	I want see my 10 daily matches after I complete creating my profile
 
 
-Background: the profile creation form is shown
+Scenario: the profile creation
 
 	Given  I am on the home page
-	And    the following questions exist: phone, password
-
-
-Scenario: fill in correct answers
-	Given  I fill in the following questions: Phone, Name, Age, Gender, Sexuality, Height, Location, Education, Career, Hobbies
+	And I fill in "user[phone]" with "2342342345"
+	And I fill in "user[password]" with "test1234"
 	When   I click "submit"
-	Then   I should see the match page
+	Then   I should see the user edit page
 
 
-Scenario: click submit button with incomplete answers
-	Given  I fill in the following questions: Phone, Name, Age, Gender, Sexuality, Height, Location
-	When   I click "submit"
-	Then   I should see a warning "You have incomplete answers for some questions!"
-	And    I should see the following questions highlighted: Location, Education, Career, Hobbies
-
-
-Scenario: click submit button with a invalid answer for Phone
-	Given  I fill in the question "Phone" with answer "abcde"
-	And    I fill in the following questions: Name, Age, Gender, Sexuality, Height, Location, Education, Career, Hobbies
-	When   I click "submit"
-	Then   I should see a warning "You have entered an invalid Phone Number"
-	And    I should see the following questions highlighted: Phone
+#Scenario: fill in correct answers
+#	Given  I fill in the following questions: Phone, Name, Age, Gender, Sexuality, Height, Location, Education, Career, Hobbies
+#	When   I click "submit"
+#	Then   I should see the match page
+#
+#
+#Scenario: click submit button with incomplete answers
+#	Given  I fill in the following questions: Phone, Name, Age, Gender, Sexuality, Height, Location
+#	When   I click "submit"
+#	Then   I should see a warning "You have incomplete answers for some questions!"
+#	And    I should see the following questions highlighted: Location, Education, Career, Hobbies
+#
+#
+#Scenario: click submit button with a invalid answer for Phone
+#	Given  I fill in the question "Phone" with answer "abcde"
+#	And    I fill in the following questions: Name, Age, Gender, Sexuality, Height, Location, Education, Career, Hobbies
+#	When   I click "submit"
+#	Then   I should see a warning "You have entered an invalid Phone Number"
+#	And    I should see the following questions highlighted: Phone
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,28 +20,51 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  it "should not update profile if career field was blank" do
-    get "/users/new"
-    expect(response).to render_template(:new)
+  describe '#create' do
+    context 'all valid form attributes are present' do
+      it 'creates the user profile successfully' do
+        post "/users", { :user => { :phone => "4123423455", :password => "1234qwer" } }
 
-    post "/users", { :user => { :phone => "4123423452", :password => "1234qwer" } }
+        expect(response).to redirect_to(edit_user_path(User.last))
+        follow_redirect!
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
 
-    expect(response).to redirect_to(edit_user_path(User.last))
-    follow_redirect!
-    expect(response).to render_template(:edit)
+  describe '#update' do
+    context 'career field is missing in the form attributes' do
+      user = User.last ||  User.create(phone: "4123423452", password: "1234qwer")
+      # let's intentionally remove career field,
+      # which is required to be present
+      # and checked by User model validations
+      fields = EXAMPLE_INPUT_FIELDS.clone
+      fields.delete(:career)
 
-    # let's intentionally remove career field,
-    # which is required to be present
-    # and checked by User model validations
-    fields = EXAMPLE_INPUT_FIELDS.clone
-    fields.delete(:career)
+      it 'should not update the user  & display error message' do
+        get "/users/#{user.id}/edit"
 
-    # this request should not update our user,
-    # since required field was not provided
-    put "/users/#{User.last.id}", { :user => fields }
-    expect(response).to render_template(:edit)
+        # this request should not update our user,
+        # since required field was not provided
+        put "/users/#{user.id}", { :user => fields }
 
-    # check that error message was rendered
-    expect(response.body).to match("can&#39;t be blank")
+        expect(response).to render_template(:edit)
+
+        # check that error message was rendered
+        expect(response.body).to match("can&#39;t be blank")
+      end
+    end
+
+    context 'all required user fields are present' do
+      user = User.last ||  User.create(phone: "4123423452", password: "1234qwer")
+
+      it 'should update the user successfully' do
+        get "/users/#{user.id}/edit"
+        put "/users/#{user.id}", { :user => EXAMPLE_INPUT_FIELDS }
+        expect(response).to redirect_to(matchmake_index_path)
+        expect(user.reload.name).to eq(EXAMPLE_INPUT_FIELDS[:name])
+        expect(user.reload.gender).to eq(EXAMPLE_INPUT_FIELDS[:gender])
+      end
+    end
   end
 end
