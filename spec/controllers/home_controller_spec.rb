12fe6efd4ddf266ffa4f66c36683f4ef16e26756
Jack Wang <jackwang@Jacks-MacBook-Pro-5.local>
2022-11-14 16:58:53 -0500

    rspec tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  pending describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # pending describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     #expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
