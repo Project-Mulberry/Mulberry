3eff53e1dc570a14f8b05f98a1c9b233ac4d23a0
Hang Yuan <hyuan3@ucsc.edu>
2022-11-16 22:46:17 -0500

    test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,9 +21,6 @@ class MessagesController < ApplicationController
     Coupon.all.to_a.each do |coupon|
       @coupons[coupon["cid"]] = coupon
     end
-    puts '--------------------'
-    puts @coupons
-    puts '--------------------'
   end
 
   # GET /messages/new
